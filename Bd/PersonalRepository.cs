eda5e43e2493a7b37f760b858d8b74a1f77548ab
Alan Adair Morgado Morales <AlanMorgadoMORALES1998@outlook.com>
2022-06-16 08:19:14 -0500

    Se obtiene una lista de los ajustadores existentes en la BD

---
+++ Bd/PersonalRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SigesivServer.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SigesivServer.Bd
+{
+    public class PersonalRepository : ConexionBD
+    {
+        public async Task<ActionResult<List<ViewModelPersonalAjustadores>>> consultarTodosLosAjustadores()
+        {
+
+            try
+            {
+                List<ViewModelPersonalAjustadores> personalesAjustador = new List<ViewModelPersonalAjustadores>();
+                var personalAjustador = conexion.personalAjustador.FromSqlInterpolated($@"EXEC sp_obtenerTodosLosAjustadores").AsAsyncEnumerable();
+
+                await foreach (var ajustador in personalAjustador)
+                {
+                    personalesAjustador.Add(ajustador);
+                }
+                return personalesAjustador;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            return null;
+
+        }
+    }
+}
---
+++ Models/Respuestas/RespuestaObtenerTodosLosAjustadores.cs
@@ -0,0 +1,13 @@
+﻿using SigesivServer.Models.ViewModels;
+using System.Collections.Generic;
+
+namespace SigesivServer.Models.Respuestas
+{
+    public class RespuestaObtenerTodosLosAjustadores
+    {
+        public int status { get; set; }
+        public string mensaje { get; set; }
+        public List<ViewModelPersonalAjustadores> data { get; set; }
+        public List<string> errores { get; set; }
+    }
+}
