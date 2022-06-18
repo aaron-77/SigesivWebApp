3d11747a0c1e71d9e6b6fa8bef008fcc90e262ab
Alan Adair Morgado Morales <AlanMorgadoMORALES1998@outlook.com>
2022-06-18 18:14:00 -0500

    Se recupera llena la tabla de Ajustadores y de Reportes sin ajustador en la vista

---
+++ Models/Respuestas/RespuestaReportesSinAsignar_Ajustadores.cs
@@ -0,0 +1,13 @@
+﻿using SigesivServer.Models.ViewModels;
+using System.Collections.Generic;
+
+namespace SigesivServer.Models.Respuestas
+{
+    public class RespuestaReportesSinAsignar_Ajustadores
+    {
+        public int status { get; set; }
+        public string mensaje { get; set; }
+        public ViewModelReportesSinAsignar_Ajustadores data { get; set; }
+        public List<string> errores { get; set; }
+    }
+}
