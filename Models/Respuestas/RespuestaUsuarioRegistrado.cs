cb19d96f6c46791fc4cb9ebe278f591dde395eec
Alan Adair Morgado Morales <AlanMorgadoMORALES1998@outlook.com>
2022-06-19 23:07:45 -0500

    Se escribio la logica de incio de sesion para el sistema, en el cual se redirige al menu dependiendo de que tipo de usuario sea.

---
+++ Models/Respuestas/RespuestaUsuarioRegistrado.cs
@@ -0,0 +1,14 @@
+﻿using SigesivServer.Models.ViewModels;
+using System.Collections.Generic;
+
+namespace SigesivServer.Models.Respuestas
+{
+    public class RespuestaUsuarioRegistrado
+    {
+        public int status { get; set; }
+        public string mensaje { get; set; }
+        public ViewModelUsuarioRegistrado data { get; set; }
+        public List<string> errores { get; set; }
+
+    }
+}
