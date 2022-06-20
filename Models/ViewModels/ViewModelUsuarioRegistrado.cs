e9850b683fc50626d14347f62b663278584aeb85
Alan Adair Morgado Morales <AlanMorgadoMORALES1998@outlook.com>
2022-06-19 23:19:08 -0500

    ViewModel para el inicio de sesion correcto

---
+++ Models/ViewModels/ViewModelUsuarioRegistrado.cs
@@ -0,0 +1,10 @@
+﻿namespace SigesivServer.Models.ViewModels
+{
+    public class ViewModelUsuarioRegistrado
+    {
+        public int id { get; set; }
+        public int fkRol { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
